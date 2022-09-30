936e979771683b1fae04c7f526ec794aa34de4d9
kyungwon3 <100257397+kyungwon3@users.noreply.github.com>
2022-09-30 12:02:30 +0900

    Create pvpvc.sh

---
+++ test/pvpvc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir ${pagename}pvpvc
+echo " 
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: ${pagename}vol
+spec:
+  capacity:
+    storage: 100Mi
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Retain
+  nfs:
+    server: 192.168.1.101
+    path: /${pagename} " > ~/test/${pagename}pvpvc/${pagename}pv.yml
+
+
+echo "
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: ${pagename}pvc
+spec:
+  accessModes:
+    - ReadWriteMany
+  resources:
+    requests:
+      storage: 50Mi" > ~/test/${pagename}pvpvc/${pagename}pvc.yml
+
+
+kubectl apply -f ~/test/${pagename}pvpvc/.
