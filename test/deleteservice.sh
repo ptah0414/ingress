a8d7244ce7816378b764506a80c5e64ec6dfb243
kyungwon3 <100257397+kyungwon3@users.noreply.github.com>
2022-10-02 14:50:45 +0900

    Update deleteservice.sh

--- test/deleteservice.sh
+++ test/deleteservice.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 b=1
+c=default
 # deleting -> $1, deletepagename -> $2
+
 if [ $1 -eq $b ];
 then
-        if [ $2 = "default"]
-        then
-                rm -rf $2
-                kubectl delete cm ${2}cm
-                kubectl delete deployment ${2}
-                kubectl delete svc ${2}
-        else
-                rm -rf $2
-                kubectl delete cm ${2}cm
-                kubectl delete deployment ${2}
-                kubectl delete svc ${2}
-                grep -n $2 ~/ingress/test/ingress-config.yml > a.txt
-                d=`awk -F : '{print $1}' a.txt`
-                e=`expr $d + 6`
-                sed "$d, ${e}d" ~/ingress/test/ingress-config.yml > b.txt
-                cat b.txt > ~/ingress/test/ingress-config.yml
-        fi
-else
+        rm -rf $2
+        kubectl delete cm ${2}cm
+        kubectl delete deployment ${2}
+        kubectl delete svc ${2}
+        grep -n /${2} ~/ingress/test/ingress-config.yml | tail -1 > a.txt
+        d=`awk -F : '{print $1}' a.txt`
+        e=`expr $d + 6`
+        sed "$d, ${e}d" ~/ingress/test/ingress-config.yml > b.txt
+        cat b.txt > ~/ingress/test/ingress-config.yml
+
+fi
